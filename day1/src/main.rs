20fb1013644ca27e0d08b7e5947669ed53cc027a
Jacques Schutte <jqschutte@gmail.com>
2019-12-01 07:08:33 +0200

    Day 1 - Part 1

--- day1/src/main.rs
+++ day1/src/main.rs
@@ -1,3 +1,11 @@
-fn main() {
-    println!("Hello, world!");
+fn main()
+{
+    let input = include_str!("../input");
+
+    let day1 = input.lines()
+                    .map(|l| l.parse::<i32>().unwrap())
+                    .map(|n| (n/3) - 2)
+                    .sum::<i32>();
+
+    println!("Day 1, Part 1: {:#?}", day1);
 }
