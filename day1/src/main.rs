cd4c44e7336e4ea02d37e8fd5d9653a6e447cf61
Jacques Schutte <jqschutte@gmail.com>
2019-12-01 07:27:27 +0200

    Formatting

--- day1/src/main.rs
+++ day1/src/main.rs
@@ -1,34 +1,32 @@
-fn fuel_required(m: i32) -> i32
-{
-    (m/3) - 2
+fn fuel_required(m: i32) -> i32 {
+    (m / 3) - 2
 }
 
-fn extra_fuel_required(m: i32) -> i32
-{
+fn extra_fuel_required(m: i32) -> i32 {
     println!("{}", m);
     let f = fuel_required(m);
-    match f
-    {
+    match f {
         f if f <= 0 => 0,
-        _      => f + extra_fuel_required(f)
+        _ => f + extra_fuel_required(f),
     }
 }
 
-fn main()
-{
+fn main() {
     let input = include_str!("../input");
 
-    let part1 = input.lines()
-                     .map(|l| l.parse::<i32>().unwrap())
-                     .map(fuel_required)
-                     .sum::<i32>();
+    let part1 = input
+        .lines()
+        .map(|l| l.parse::<i32>().unwrap())
+        .map(fuel_required)
+        .sum::<i32>();
 
     println!("Day 1, Part 1: {:#?}", part1);
 
-    let part2 = input.lines()
-                     .map(|l| l.parse::<i32>().unwrap())
-                     .map(extra_fuel_required)
-                     .sum::<i32>();
+    let part2 = input
+        .lines()
+        .map(|l| l.parse::<i32>().unwrap())
+        .map(extra_fuel_required)
+        .sum::<i32>();
 
     println!("Day 1, Part 2: {:#?}", part2);
 }
