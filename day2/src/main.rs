061a95e58579601c604933ea8c363fe78ee45b0a
jacquess <jacquess@bbd.co.za>
2019-12-02 08:29:24 +0200

    Day 2 - Part 1

--- day2/src/main.rs
+++ day2/src/main.rs
@@ -1,3 +1,28 @@
+use std::cell::Cell;
+
 fn main() {
-    println!("Hello, world!");
+    let input = include_str!("../input");
+
+    let program: Vec<_> = input
+        .trim()
+        .split(",")
+        .map(|s| Cell::new(s.parse::<usize>().unwrap()))
+        .collect();
+
+    program[1].set(12);
+    program[2].set(2);
+
+    for n in program.chunks(4)
+    {
+        println!("{:?}", n);
+        match n[0].get()
+        {
+            1 => program[n[3].get()].set(program[n[1].get()].get() + program[n[2].get()].get()),
+            2 => program[n[3].get()].set(program[n[1].get()].get() * program[n[2].get()].get()),
+            99 => break,
+            _ => (),
+        }
+    }
+
+    println!("Part 1: {}", program[0].get());
 }
