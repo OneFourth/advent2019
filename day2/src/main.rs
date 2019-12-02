81fd3685022095a5c942f445142c4b44ada60cd4
jacquess <jacquess@bbd.co.za>
2019-12-02 08:38:51 +0200

    Day 2 - Part 2

--- day2/src/main.rs
+++ day2/src/main.rs
@@ -1,28 +1,50 @@
 use std::cell::Cell;
 
-fn main() {
-    let input = include_str!("../input");
-
-    let program: Vec<_> = input
-        .trim()
-        .split(",")
-        .map(|s| Cell::new(s.parse::<usize>().unwrap()))
-        .collect();
-
-    program[1].set(12);
-    program[2].set(2);
-
+fn run_program(program: &Vec<Cell<usize>>) -> usize
+{
     for n in program.chunks(4)
     {
-        println!("{:?}", n);
         match n[0].get()
         {
             1 => program[n[3].get()].set(program[n[1].get()].get() + program[n[2].get()].get()),
             2 => program[n[3].get()].set(program[n[1].get()].get() * program[n[2].get()].get()),
             99 => break,
             _ => (),
-        }
+        };
     }
 
-    println!("Part 1: {}", program[0].get());
+    return program[0].get()
+}
+
+fn main() {
+    let input = include_str!("../input");
+
+    let base: Vec<_> = input
+        .trim()
+        .split(",")
+        .map(|s| Cell::new(s.parse::<usize>().unwrap()))
+        .collect();
+
+    let program = base.clone();
+
+    program[1].set(12);
+    program[2].set(2);
+
+    println!("Part 1: {}", run_program(&program));
+
+    let target = 19690720;
+
+    for verb in 0..99
+    {
+        for noun in 0..99
+        {
+            let clone = base.clone();
+            clone[1].set(noun);
+            clone[2].set(verb);
+            if run_program(&clone) == target
+            {
+                println!("Part 2: {}", 100 * noun + verb);
+            }
+        }
+    }
 }
