b2bf2d9e4f3ef8189b2e532f167179b0c02f6adb
jacquess <jacquess@bbd.co.za>
2019-12-03 09:47:52 +0200

    Day 3 - Part 2 - Fixed up a bit

--- day3/src/main.rs
+++ day3/src/main.rs
@@ -11,46 +11,40 @@ fn build_wire(input_str: &str) -> Wire {
     let mut x = 0;
     let mut y = 0;
 
-    for dir in input_str.trim().split(',')
-    {
+    for dir in input_str.trim().split(',') {
         let (head, tail) = dir.split_at(1);
         let count = tail.parse::<i32>().unwrap();
-        let (a_x, a_y) = match head
-            {
-                "U" => ( 0,  1),
-                "D" => ( 0, -1),
-                "L" => (-1,  0),
-                "R" => ( 1,  0),
-                _ => (0, 0)
-            };
+        let (a_x, a_y) = match head {
+            "U" => (0, 1),
+            "D" => (0, -1),
+            "L" => (-1, 0),
+            "R" => (1, 0),
+            _ => (0, 0),
+        };
 
-        for _ in 1..count+1 {
+        for _ in 1..count + 1 {
             x += a_x;
             y += a_y;
 
-            wire.push((x,y));
+            wire.push((x, y));
         }
     }
     wire
 }
 
-fn output(wires: Vec<Wire>)
-{
+fn output(wires: Vec<Wire>) {
     let mut out: Vec<Vec<char>> = vec![vec!['.'; 1000]; 1000];
     out[300][300] = 'o';
 
     let mut count = 1;
-    for w in wires
-    {
-        for (x, y) in w
-        {
-            out[(y+300) as usize][(x+300) as usize] = std::char::from_digit(count, 10).unwrap();
+    for w in wires {
+        for (x, y) in w {
+            out[(y + 300) as usize][(x + 300) as usize] = std::char::from_digit(count, 10).unwrap();
         }
         count += 1;
     }
 
-    for s in out
-    {
+    for s in out {
         println!("{:?}", s.into_iter().collect::<String>());
     }
 }
@@ -61,21 +55,22 @@ fn main() {
     let wires: Vec<_> = input.lines().map(|s| build_wire(s)).collect();
     let sets: Vec<HashSet<_>> = wires.iter().map(|w| HashSet::from_iter(w)).collect();
     let intersections: Vec<_> = sets[0].intersection(&sets[1]).collect();
-    let distances: Vec<_> = intersections.iter().map(|(x, y)| x.abs() + y.abs()).collect();
+    let distances: Vec<_> = intersections
+        .iter()
+        .map(|(x, y)| x.abs() + y.abs())
+        .collect();
 
     println!("Part 1: {}", distances.iter().min().unwrap());
 
-    for i in &intersections
-    {
-        let mut total = 0;
-        for w in &wires
-        {
-            let val = w.iter().position(|&p| p == ***i).unwrap() + 1;
-            total += val;
-        }
-
-        println!("{}", total);
-    }
-
-    //output(wires);
+    let min: usize = intersections
+        .iter()
+        .map(|&&i| {
+            wires
+                .iter()
+                .map(|w| w.iter().position(|p| p == i).unwrap() + 1)
+                .sum()
+        })
+        .min()
+        .unwrap();
+    println!("Part 2: {}", min);
 }
