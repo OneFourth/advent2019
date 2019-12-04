90fe3b25572689377a55c1bfcd03322f4b64f47f
jacquess <jacquess@bbd.co.za>
2019-12-04 07:36:09 +0200

    Day 4 - Part 1

--- day4/src/main.rs
+++ day4/src/main.rs
@@ -1,3 +1,24 @@
+fn is_valid(s: usize) -> bool {
+    let val: Vec<_> = s.to_string().chars().map(|d| d.to_digit(10).unwrap()).collect();
+    (val[0] <= val[1]
+        && val[1] <= val[2]
+        && val[2] <= val[3]
+        && val[3] <= val[4]
+        && val[4] <= val[5])
+        && (val[0] == val[1]
+            || val[1] == val[2]
+            || val[2] == val[3]
+            || val[3] == val[4]
+            || val[4] == val[5])
+}
+
 fn main() {
-    println!("Hello, world!");
+    let input = include_str!("../input");
+    let split: Vec<_> = input
+        .split("-")
+        .map(|s| s.trim().parse::<usize>().unwrap())
+        .collect();
+    let (low, high) = (split[0], split[1]);
+
+    println!("Part 1: {}", (low..=high).filter(|&n| is_valid(n)).count());
 }
