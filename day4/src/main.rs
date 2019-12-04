a7096d2b08632ff1a9d044ae792f1cdeb38656fc
jacquess <jacquess@bbd.co.za>
2019-12-04 07:55:59 +0200

    Day 4 - Part 2

--- day4/src/main.rs
+++ day4/src/main.rs
@@ -1,5 +1,11 @@
-fn is_valid(s: usize) -> bool {
-    let val: Vec<_> = s.to_string().chars().map(|d| d.to_digit(10).unwrap()).collect();
+use std::collections::HashMap;
+
+fn is_valid_part1(s: usize) -> bool {
+    let val: Vec<_> = s
+        .to_string()
+        .chars()
+        .map(|d| d.to_digit(10).unwrap())
+        .collect();
     (val[0] <= val[1]
         && val[1] <= val[2]
         && val[2] <= val[3]
@@ -12,6 +18,28 @@ fn is_valid(s: usize) -> bool {
             || val[4] == val[5])
 }
 
+fn is_valid_part2(s: usize) -> bool {
+    let val: Vec<_> = s
+        .to_string()
+        .chars()
+        .map(|d| d.to_digit(10).unwrap())
+        .collect();
+    let mut counts = HashMap::new();
+
+    for n in val.windows(2) {
+        if n[0] == n[1] {
+            *counts.entry(n[0]).or_insert(0) += 1;
+        }
+    }
+
+    (val[0] <= val[1]
+        && val[1] <= val[2]
+        && val[2] <= val[3]
+        && val[3] <= val[4]
+        && val[4] <= val[5])
+        && counts.values().any(|&x| x == 1)
+}
+
 fn main() {
     let input = include_str!("../input");
     let split: Vec<_> = input
@@ -20,5 +48,12 @@ fn main() {
         .collect();
     let (low, high) = (split[0], split[1]);
 
-    println!("Part 1: {}", (low..=high).filter(|&n| is_valid(n)).count());
+    println!(
+        "Part 1: {}",
+        (low..=high).filter(|&n| is_valid_part1(n)).count()
+    );
+    println!(
+        "Part 2: {}",
+        (low..=high).filter(|&n| is_valid_part2(n)).count()
+    );
 }
