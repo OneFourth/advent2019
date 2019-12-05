6f9d5094461c74ffb27e6ea4bf06c87d60f80391
jacquess <jacquess@bbd.co.za>
2019-12-05 15:44:30 +0200

    Day 5 - Part 2 redone

--- day5/src/main.rs
+++ day5/src/main.rs
@@ -1,13 +1,5 @@
 use std::cell::Cell;
 
-fn get_parm(pos: usize, digits: &[char], pointer: usize, program: &[Cell<i32>]) -> i32 {
-    if digits.len() > pos + 1 && digits[pos + 1] == '1' {
-        program[pointer + pos].get() // value
-    } else {
-        program[program[pointer + pos].get() as usize].get() // address
-    }
-}
-
 fn run_program(program: &[Cell<i32>], input: i32) {
     let mut pointer = 0;
     while pointer < program.len() {
@@ -17,61 +9,46 @@ fn run_program(program: &[Cell<i32>], input: i32) {
             .chars()
             .rev()
             .collect::<Vec<_>>();
+
+        let get_cell = |a: usize| &program[program[pointer + a].get() as usize];
+        let get_parm = |pos: usize| -> i32 {
+            if digits.len() > pos + 1 && digits[pos + 1] == '1' {
+                program[pointer + pos].get() // value
+            } else {
+                get_cell(pos).get() // address
+            }
+        };
+
+        let before_op = pointer;
         match digits[0] {
-            '1' => {
-                program[program[pointer + 3].get() as usize].set(
-                    get_parm(1, &digits, pointer, program) + get_parm(2, &digits, pointer, program),
-                );
-                pointer += 4;
-            }
-            '2' => {
-                program[program[pointer + 3].get() as usize].set(
-                    get_parm(1, &digits, pointer, program) * get_parm(2, &digits, pointer, program),
-                );
-                pointer += 4;
-            }
-            '3' => {
-                program[program[pointer + 1].get() as usize].set(input);
-                pointer += 2;
-            }
-            '4' => {
-                println!(
-                    "Output: {}",
-                    program[program[pointer + 1].get() as usize].get()
-                );
-                pointer += 2;
-            }
+            '1' => get_cell(3).set(get_parm(1) + get_parm(2)),
+            '2' => get_cell(3).set(get_parm(1) * get_parm(2)),
+            '3' => get_cell(1).set(input),
+            '4' => println!("Output: {}", get_cell(1).get()),
             '5' => {
-                if get_parm(1, &digits, pointer, program) != 0 {
-                    pointer = get_parm(2, &digits, pointer, program) as usize;
-                } else {
-                    pointer += 3;
+                if get_parm(1) != 0 {
+                    pointer = get_parm(2) as usize
                 }
             }
             '6' => {
-                if get_parm(1, &digits, pointer, program) == 0 {
-                    pointer = get_parm(2, &digits, pointer, program) as usize;
-                } else {
-                    pointer += 3;
+                if get_parm(1) == 0 {
+                    pointer = get_parm(2) as usize
                 }
             }
-            '7' => {
-                program[program[pointer + 3].get() as usize].set(
-                    (get_parm(1, &digits, pointer, program)
-                        < get_parm(2, &digits, pointer, program)) as i32,
-                );
-                pointer += 4;
-            }
-            '8' => {
-                program[program[pointer + 3].get() as usize].set(
-                    (get_parm(1, &digits, pointer, program)
-                        == get_parm(2, &digits, pointer, program)) as i32,
-                );
-                pointer += 4;
-            }
+            '7' => get_cell(3).set((get_parm(1) < get_parm(2)) as i32),
+            '8' => get_cell(3).set((get_parm(1) == get_parm(2)) as i32),
             '9' => break,
             _ => panic!("help"),
         };
+
+        if pointer == before_op {
+            pointer += match digits[0] {
+                '3' | '4' => 2,
+                '5' | '6' => 3,
+                '1' | '2' | '7' | '8' => 4,
+                _ => 1,
+            };
+        }
     }
 }
 
