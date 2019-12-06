d809abbf9bcb28f20030a3ec560e7af0d8d409b2
jacquess <jacquess@bbd.co.za>
2019-12-06 08:20:33 +0200

    Day 6 - Part 1

--- day6/src/main.rs
+++ day6/src/main.rs
@@ -1,3 +1,33 @@
+use std::collections::HashMap;
+
+fn counter(mapping: &HashMap<String, String>, k: &String) -> usize {
+    let mut count = 0;
+    let mut search = k;
+
+    loop {
+        let found = mapping.get(search);
+        match &found {
+            None => break,
+            Some(v) => {
+                count += 1;
+                search = &v;
+            }
+        }
+    }
+
+    count
+}
+
 fn main() {
-    println!("Hello, world!");
+    let input = include_str!("../input");
+
+    let mapping = input
+        .lines()
+        .map(|s| (s[4..7].to_string(), s[0..3].to_string()))
+        .collect::<HashMap<_, _>>();
+
+    println!(
+        "Part 1: {}",
+        mapping.keys().map(|k| counter(&mapping, &k)).sum::<usize>()
+    );
 }
