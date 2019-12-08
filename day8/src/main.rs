2b5322b0f9e4e4fb91a2146babbda488a48cf553
Jacques Schutte <jqschutte@gmail.com>
2019-12-08 07:52:29 +0200

    Day 8 - Part 2

--- day8/src/main.rs
+++ day8/src/main.rs
@@ -1,12 +1,25 @@
-#[derive(Debug)]
+use std::ops;
+
+#[derive(Debug, Clone)]
 struct Layer
 {
-    width: usize,
-    height: usize,
-
     pixels: Vec<u32>,
 }
 
+impl ops::Add<Layer> for Layer {
+    type Output = Layer;
+
+    fn add(self, _rhs: Layer) -> Layer {
+        Layer {
+            pixels: self.pixels.iter().zip(_rhs.pixels.iter()).map(|(a, b)| {
+                match (a, b) {
+                    (2, _) => *b,
+                    (_, _) => *a,
+                }
+            }).collect(),
+        }
+    }
+}
 
 fn main() {
     let input = include_str!("../input").trim();
@@ -14,13 +27,34 @@ fn main() {
 
     let width = 25;
     let height = 6;
-    let mut layers: Vec<_> = chars.chunks(width*height).into_iter().map(|c| Layer{
-        width: width,
-        height: height,
+    let layers: Vec<_> = chars.chunks(width*height).into_iter().map(|c| Layer{
         pixels: c.iter().map(|x| x.to_digit(10).unwrap()).collect(),
     }).collect();
 
-    layers.sort_by(|a, b| a.pixels.iter().filter(|&&p| p == 0).count().partial_cmp(&b.pixels.iter().filter(|&&p| p == 0).count()).unwrap());
+    {
+        let mut part1 = layers.clone();
 
-    println!("Part 1: {:?}", layers[0].pixels.iter().filter(|&&p| p == 1).count() * layers[0].pixels.iter().filter(|&&p| p == 2).count());
+        part1.sort_by(|a, b| a.pixels.iter().filter(|&&p| p == 0).count().partial_cmp(&b.pixels.iter().filter(|&&p| p == 0).count()).unwrap());
+
+        println!("Part 1: {:?}", part1[0].pixels.iter().filter(|&&p| p == 1).count() * part1[0].pixels.iter().filter(|&&p| p == 2).count());
+    }
+
+    {
+        let part2 = layers.clone();
+
+        let mut layer = part2.first().unwrap().clone();
+        for n in 1..part2.len() {
+            layer = layer + part2[n].clone();
+        }
+
+        println!("Part 2:");
+        for c in layer.pixels.chunks(25)
+        {
+            for p in c
+            {
+                print!("{}", p);
+            }
+            print!("\n");
+        }
+    }
 }
