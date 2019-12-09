d29abc92d97df730cf01d9ad573622ac65d69fed
jacquess <jacquess@bbd.co.za>
2019-12-09 12:15:51 +0200

    Day 1 - simplified + tests

--- day1/src/main.rs
+++ day1/src/main.rs
@@ -3,7 +3,6 @@ fn fuel_required(m: i32) -> i32 {
 }
 
 fn extra_fuel_required(m: i32) -> i32 {
-    println!("{}", m);
     let f = fuel_required(m);
     match f {
         f if f <= 0 => 0,
@@ -11,22 +10,45 @@ fn extra_fuel_required(m: i32) -> i32 {
     }
 }
 
+fn parse(input: &str) -> Vec<i32> {
+    input.lines().map(|l| l.parse::<i32>().unwrap()).collect()
+}
+
+fn part1(input: &str) -> i32 {
+    parse(input).into_iter().map(fuel_required).sum()
+}
+
+fn part2(input: &str) -> i32 {
+    parse(input).into_iter().map(extra_fuel_required).sum()
+}
+
 fn main() {
     let input = include_str!("../input");
 
-    let part1 = input
-        .lines()
-        .map(|l| l.parse::<i32>().unwrap())
-        .map(fuel_required)
-        .sum::<i32>();
+    println!("Part 1: {}", part1(input));
+    println!("Part 2: {}", part2(input));
+}
 
-    println!("Day 1, Part 1: {:#?}", part1);
+#[cfg(test)]
+mod tests {
+    use super::*;
 
-    let part2 = input
-        .lines()
-        .map(|l| l.parse::<i32>().unwrap())
-        .map(extra_fuel_required)
-        .sum::<i32>();
+    #[test]
+    fn test_part1() {
+        assert_eq!(part1("12"), 2);
+        assert_eq!(part1("14"), 2);
+        assert_eq!(part1("1969"), 654);
+        assert_eq!(part1("100756"), 33583);
 
-    println!("Day 1, Part 2: {:#?}", part2);
+        assert_eq!(part1(include_str!("../input")), 3305115);
+    }
+
+    #[test]
+    fn test_part2() {
+        assert_eq!(part2("14"), 2);
+        assert_eq!(part2("1969"), 966);
+        assert_eq!(part2("100756"), 50346);
+
+        assert_eq!(part2(include_str!("../input")), 4954799);
+    }
 }
