f9bb99abad526fd864a1d90e58c181e86874228d
jacquess <jacquess@bbd.co.za>
2019-12-11 09:10:36 +0200

    Day 11 - Part 2

--- day11/src/main.rs
+++ day11/src/main.rs
@@ -113,13 +113,12 @@ impl Thruster {
 fn turn_to(dir: i64, turn: i64) -> i64 {
     if turn == 0 {
         (dir + 3) % 4
-    }
-    else {
+    } else {
         (dir + 1) % 4
     }
 }
 
-fn run(base: &[Cell<i64>]) {
+fn part1(base: &[Cell<i64>]) {
     let mut t = Thruster {
         pointer: 0,
         done: false,
@@ -137,7 +136,7 @@ fn run(base: &[Cell<i64>]) {
     let mut count = 0;
 
     while !t.done {
-        let mut entry = painted.entry(position).or_insert_with(|| {
+        let entry = painted.entry(position).or_insert_with(|| {
             count += 1;
             0
         });
@@ -155,6 +154,56 @@ fn run(base: &[Cell<i64>]) {
     println!("{}", count);
 }
 
+fn part2(base: &[Cell<i64>]) {
+    let mut t = Thruster {
+        pointer: 0,
+        done: false,
+        relative: 0,
+        input: vec![],
+        program: base.to_owned(),
+    };
+
+    let directions = vec![(0, 1), (1, 0), (0, -1), (-1, 0)];
+
+    let mut painted = HashMap::new();
+    let mut position = (0, 0);
+    painted.insert(position, 1);
+    let mut direction = 0;
+
+    while !t.done {
+        let entry = painted.entry(position).or_insert(0);
+        t.input.push(*entry);
+        if let Some(colour) = t.run_program() {
+            if let Some(turn) = t.run_program() {
+                *entry = colour;
+                direction = turn_to(direction, turn);
+                let move_dir = directions[direction as usize];
+                position = (position.0 + move_dir.0, position.1 + move_dir.1);
+            };
+        }
+    }
+
+    let x_min = painted.keys().min_by_key(|(x, _)| x).unwrap().0;
+    let x_max = painted.keys().max_by_key(|(x, _)| x).unwrap().0;
+    let y_min = painted.keys().min_by_key(|(_, y)| y).unwrap().1;
+    let y_max = painted.keys().max_by_key(|(_, y)| y).unwrap().1;
+
+    for y in (y_min..=y_max).rev() {
+        for x in x_min..=x_max {
+            let entry = painted.entry((x, y)).or_insert(0);
+            print!(
+                "{}",
+                match *entry {
+                    0 => "  ",
+                    1 => "##",
+                    _ => "  ",
+                }
+            );
+        }
+        println!();
+    }
+}
+
 fn main() {
     let input = include_str!("../input");
 
@@ -164,6 +213,6 @@ fn main() {
         .map(|s| Cell::new(s.parse::<i64>().unwrap()))
         .collect();
 
-    run(&base);
+    part1(&base);
+    part2(&base);
 }
-
