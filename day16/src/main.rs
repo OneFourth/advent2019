0fd70d7c1d4c55230b0356829f67bc692821fba0
Jacques Schutte <jqschutte@gmail.com>
2019-12-16 11:22:54 +0200

    Day 16 - Part 2

--- day16/src/main.rs
+++ day16/src/main.rs
@@ -15,7 +15,15 @@ fn phase(numbers: Vec<isize>) -> Vec<isize> {
     let mut vals = vec![];
     for i in 0..=numbers.len() {
         let pat = get_pattern(i, numbers.len());
-        vals.push(numbers.iter().zip(pat.iter()).map(|(n, p)| n * p).sum::<isize>().abs() % 10);
+        vals.push(
+            numbers
+                .iter()
+                .zip(pat.iter())
+                .map(|(n, p)| n * p)
+                .sum::<isize>()
+                .abs()
+                % 10,
+        );
     }
     vals
 }
@@ -28,6 +36,24 @@ fn part1(numbers: &Vec<isize>) -> Vec<isize> {
     current.into_iter().take(8).collect::<Vec<_>>()
 }
 
+fn part2(numbers: &Vec<isize>) -> Vec<isize> {
+    let offset = numbers[..7].iter().fold(0, |n, &d| 10 * n + d) as usize;
+    let mut actual_input: Vec<_> = numbers.clone()
+        .into_iter()
+        .cycle()
+        .skip(offset % numbers.len())
+        .take(((numbers.len() as isize * 10_000) - offset as isize) as usize)
+        .collect();
+    for _ in 0..100 {
+        let mut partial = 0;
+        for n in (0..actual_input.len()).rev() {
+            partial = (partial + actual_input[n]) % 10;
+            actual_input[n] = partial;
+        }
+    }
+    actual_input.into_iter().take(8).collect::<Vec<_>>()
+}
+
 fn main() {
     let input = include_str!("../input");
     let numbers: Vec<_> = input
@@ -37,4 +63,5 @@ fn main() {
         .collect();
 
     println!("{:?}", part1(&numbers));
+    println!("{:?}", part2(&numbers));
 }
