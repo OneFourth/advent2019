3bdb225bc280eb5ff4f23ed771b3c8e719a41bc6
Jacques Schutte <jqschutte@gmail.com>
2019-12-17 10:40:46 +0200

    Day 17 - Part 2

--- day17/src/main.rs
+++ day17/src/main.rs
@@ -113,7 +113,7 @@ impl Thruster {
     }
 }
 
-fn part1(base: &[Cell<i64>]) -> usize {
+fn get_map(base: &[Cell<i64>]) -> (Thruster, HashMap<(usize, usize), char>, (usize, usize)) {
     let mut t = Thruster {
         pointer: 0,
         done: false,
@@ -124,12 +124,17 @@ fn part1(base: &[Cell<i64>]) -> usize {
 
     let mut map = HashMap::new();
     let mut pos = (0, 0);
+    let mut robot_pos = pos;
 
     while let Some(out) = t.run_program() {
         let c = u8::try_from(out).unwrap() as char;
         if out != 10 {
             map.insert(pos, c);
+            if c == '^' {
+                robot_pos = pos;
+            }
             pos.0 += 1;
+
         }
         else {
             pos.1 += 1;
@@ -139,6 +144,12 @@ fn part1(base: &[Cell<i64>]) -> usize {
         print!("{}", c);
     }
 
+    (t, map, robot_pos)
+}
+
+fn part1(base: &[Cell<i64>]) -> usize {
+    let (_, map, _) = get_map(base);
+
     let x_min = map.keys().min_by_key(|(x, _)| x).unwrap().0;
     let x_max = map.keys().max_by_key(|(x, _)| x).unwrap().0;
     let y_min = map.keys().min_by_key(|(_, y)| y).unwrap().1;
@@ -146,8 +157,8 @@ fn part1(base: &[Cell<i64>]) -> usize {
 
     let mut count = 0;
 
-    for y in y_min..=y_max {
-        for x in x_min..=x_max {
+    for y in y_min+1..y_max {
+        for x in x_min+1..x_max {
             if map.get(&(x, y)) == Some(&'#') {
                 let adj = [
                     map.get(&(x - 1, y)),
@@ -167,6 +178,100 @@ fn part1(base: &[Cell<i64>]) -> usize {
     count
 }
 
+fn turn((x, y): (i32, i32), turnLeft: bool) -> (i32, i32) {
+    if turnLeft {
+        match (x, y) {
+            (-1, 0) => (0, 1),
+            (1, 0) => (0, -1),
+            (0, -1) => (-1, 0),
+            (0, 1) => (1, 0),
+            _ => panic!(),
+        }
+    }
+    else {
+        match (x, y) {
+            (-1, 0) => (0, -1),
+            (1, 0) => (0, 1),
+            (0, -1) => (1, 0),
+            (0, 1) => (-1, 0),
+            _ => panic!(),
+        }
+    }
+}
+
+#[derive(Debug)]
+enum Move {
+    TurnRight,
+    TurnLeft,
+    Move(usize),
+}
+
+fn part2(base: &[Cell<i64>]) -> i64 {
+    use crate::Move::*;
+
+    let (_, map, (mut x, mut y)) = get_map(base);
+    let mut movements = vec![];
+
+    let mut currDir = (0, -1);
+
+    let add_dir = |(x, y), (dx, dy)| {
+        ((x as i32 + dx) as usize, (y as i32 + dy) as usize)
+    };
+
+    loop {
+        let right = turn(currDir, false);
+        let left = turn(currDir, true);
+        let (new_x, new_y) = add_dir((x, y), currDir);
+        if map.get(&(new_x, new_y)) == Some(&'#') {
+            x = new_x;
+            y = new_y;
+            let mut last = movements.last_mut();
+            match last {
+                Some(Move(v)) => *v += 1,
+                _ => movements.push(Move(1)),
+            }
+        }
+        else if map.get(&add_dir((x, y), right)) == Some(&'#') {
+            currDir = right;
+            movements.push(TurnRight);
+        }
+        else if map.get(&add_dir((x, y), left)) == Some(&'#') {
+            currDir = left;
+            movements.push(TurnLeft);
+        }
+        else {
+            break;
+        }
+    }
+
+    println!("{:?}", movements);
+
+    let mut t = Thruster {
+        pointer: 0,
+        done: false,
+        relative: 0,
+        input: vec![],
+        program: base.to_owned(),
+    };
+
+    t.program[0].set(2);
+    let all = "A,B,A,C,B,C,B,C,A,C\nR,12,L,10,R,12\nL,8,R,10,R,6\nR,12,L,10,R,10,L,8\nn\n";
+    for c in all.bytes().rev() {
+        print!("{} ", c);
+        t.input.push(c as i64);
+    }
+    println!();
+
+    while let Some(v) = t.run_program() {
+        if let Ok(c) = u8::try_from(v) {
+            print!("{}", c as char);
+        }
+        else {
+            return v;
+        }
+    }
+    0
+}
 
 fn main() {
     let input = include_str!("../input");
@@ -178,4 +283,5 @@ fn main() {
         .collect();
     
     println!("Part 1: {}", part1(&base));
+    println!("Part 2: {}", part2(&base));
 }
