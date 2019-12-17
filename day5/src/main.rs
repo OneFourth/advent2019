60f721c0eaed040ae049b55eaeb214ce347529d4
Jacques Schutte <jqschutte@gmail.com>
2019-12-17 15:01:23 +0200

    Day 5 - intcode rewrite

--- day5/src/main.rs
+++ day5/src/main.rs
@@ -1,71 +1,35 @@
-use std::cell::Cell;
+use intcode::{parse_program, Computer, Program};
 
-fn run_program(program: &[Cell<i32>], input: i32) {
-    let mut pointer = 0;
-    while pointer < program.len() {
-        let digits = program[pointer]
-            .get()
-            .to_string()
-            .chars()
-            .rev()
-            .collect::<Vec<_>>();
+fn part1(base: Program) -> i64 {
+    let mut c = Computer::new(base);
 
-        let get_cell = |a: usize| &program[program[pointer + a].get() as usize];
-        let get_parm = |pos: usize| -> i32 {
-            if digits.len() > pos + 1 && digits[pos + 1] == '1' {
-                program[pointer + pos].get() // value
-            } else {
-                get_cell(pos).get() // address
-            }
-        };
+    c.push(1);
 
-        let before_op = pointer;
-        match digits[0] {
-            '1' => get_cell(3).set(get_parm(1) + get_parm(2)),
-            '2' => get_cell(3).set(get_parm(1) * get_parm(2)),
-            '3' => get_cell(1).set(input),
-            '4' => println!("Output: {}", get_cell(1).get()),
-            '5' => {
-                if get_parm(1) != 0 {
-                    pointer = get_parm(2) as usize
-                }
-            }
-            '6' => {
-                if get_parm(1) == 0 {
-                    pointer = get_parm(2) as usize
-                }
-            }
-            '7' => get_cell(3).set((get_parm(1) < get_parm(2)) as i32),
-            '8' => get_cell(3).set((get_parm(1) == get_parm(2)) as i32),
-            '9' => break,
-            _ => panic!("help"),
-        };
-
-        if pointer == before_op {
-            pointer += match digits[0] {
-                '3' | '4' => 2,
-                '5' | '6' => 3,
-                '1' | '2' | '7' | '8' => 4,
-                _ => 1,
-            };
-        }
+    let mut outputs = vec![];
+    while let Some(out) = c.run() {
+        outputs.push(out);
     }
+    if outputs.iter().rev().skip(1).any(|v| *v != 0) {
+        println!("{:?}", outputs);
+        panic!("Diagnostic failed")
+    }
+
+    *outputs.last().unwrap()
+}
+
+fn part2(base: Program) -> i64 {
+    let mut c = Computer::new(base);
+
+    c.push(5);
+
+    c.run().unwrap()
 }
 
 fn main() {
     let input = include_str!("../input");
 
-    let base: Vec<_> = input
-        .trim()
-        .split(',')
-        .map(|s| Cell::new(s.parse::<i32>().unwrap()))
-        .collect();
+    let base = parse_program(input);
 
-    let part1 = base.clone();
-    println!("Part 1");
-    run_program(&part1, 1);
-
-    println!("Part 2");
-    let part2 = base.clone();
-    run_program(&part2, 5);
+    println!("Part 1: {}", part1(base.clone()));
+    println!("Part 2: {}", part2(base.clone()));
 }
