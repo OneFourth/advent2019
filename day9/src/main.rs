2a42ed9692c46837d06f084d7c071a683c37c843
Jacques Schutte <jqschutte@gmail.com>
2019-12-17 15:46:29 +0200

    Day 9 - rewrite intcode

--- day9/src/main.rs
+++ day9/src/main.rs
@@ -1,138 +1,41 @@
-use std::cell::Cell;
+use intcode::{parse_program, Computer, Program};
 
-#[derive(Debug)]
-struct Thruster {
-    pointer: usize,
-    done: bool,
-    relative: i64,
-    input: Vec<i64>,
-    program: Vec<Cell<i64>>,
+fn part1(base: Program) -> i64 {
+    let mut c = Computer::new(base);
+    c.push(1);
+    c.run().unwrap()
 }
 
-impl Thruster {
-    fn access_program(&mut self, a: usize) -> &Cell<i64> {
-        if self.program.len() <= a {
-            self.program.resize(a + 1, Cell::new(0));
-        }
-        &self.program[a]
-    }
-
-    fn get_cell(&mut self, a: usize) -> &Cell<i64> {
-        let address = self.access_program(self.pointer + a).get() as usize;
-        &self.access_program(address)
-    }
-
-    fn get_parm(&mut self, pos: usize, digits: &[char]) -> &Cell<i64> {
-        if digits.len() > pos + 1 {
-            match digits[pos + 1] {
-                '0' => self.get_cell(pos),
-                '1' => self.access_program(self.pointer + pos), // value
-                '2' => {
-                    let address =
-                        (self.relative + self.access_program(self.pointer + pos).get()) as usize;
-                    self.access_program(address)
-                } // relative
-                _ => panic!("help get_parm"),
-            }
-        } else {
-            self.get_cell(pos) // address
-        }
-    }
-
-    fn run_program(&mut self) -> Option<i64> {
-        let mut result = None;
-        while !(self.done || result != None) {
-            let mut digits = self
-                .access_program(self.pointer)
-                .get()
-                .to_string()
-                .chars()
-                .rev()
-                .collect::<Vec<_>>();
-
-            let before_op = self.pointer;
-            if digits.len() == 1 {
-                digits.resize(2, '0');
-            }
-            match (digits[1], digits[0]) {
-                ('0', '1') => {
-                    let parm1 = self.get_parm(1, &digits).get();
-                    let parm2 = self.get_parm(2, &digits).get();
-                    self.get_parm(3, &digits).set(parm1 + parm2);
-                }
-                ('0', '2') => {
-                    let parm1 = self.get_parm(1, &digits).get();
-                    let parm2 = self.get_parm(2, &digits).get();
-                    self.get_parm(3, &digits).set(parm1 * parm2);
-                }
-                ('0', '3') => {
-                    let i = self.input.pop();
-                    self.get_parm(1, &digits).set(i.unwrap());
-                }
-                ('0', '4') => result = Some(self.get_parm(1, &digits).get()),
-                ('0', '5') => {
-                    if self.get_parm(1, &digits).get() != 0 {
-                        self.pointer = self.get_parm(2, &digits).get() as usize
-                    }
-                }
-                ('0', '6') => {
-                    if self.get_parm(1, &digits).get() == 0 {
-                        self.pointer = self.get_parm(2, &digits).get() as usize
-                    }
-                }
-                ('0', '7') => {
-                    let parm1 = self.get_parm(1, &digits).get();
-                    let parm2 = self.get_parm(2, &digits).get();
-                    self.get_parm(3, &digits).set((parm1 < parm2) as i64);
-                }
-                ('0', '8') => {
-                    let parm1 = self.get_parm(1, &digits).get();
-                    let parm2 = self.get_parm(2, &digits).get();
-                    self.get_parm(3, &digits).set((parm1 == parm2) as i64);
-                }
-                ('0', '9') => self.relative += self.get_parm(1, &digits).get(),
-                ('9', '9') => self.done = true,
-                _ => panic!("help"),
-            };
-
-            if self.pointer == before_op {
-                self.pointer += match digits[0] {
-                    '3' | '4' | '9' => 2,
-                    '5' | '6' => 3,
-                    '1' | '2' | '7' | '8' => 4,
-                    _ => 1,
-                };
-            }
-        }
-        result
-    }
-}
-
-fn run(base: &[Cell<i64>], input: Vec<i64>, part: usize) {
-    let mut t = Thruster {
-        pointer: 0,
-        done: false,
-        relative: 0,
-        input,
-        program: base.to_owned(),
-    };
-
-    while !t.done {
-        if let Some(a) = t.run_program() {
-            println!("Part {}: {}", part, a);
-        }
-    }
+fn part2(base: Program) -> i64 {
+    let mut c = Computer::new(base);
+    c.push(2);
+    c.run().unwrap()
 }
 
 fn main() {
     let input = include_str!("../input");
 
-    let base: Vec<_> = input
-        .trim()
-        .split(',')
-        .map(|s| Cell::new(s.parse::<i64>().unwrap()))
-        .collect();
+    let base = parse_program(input);
 
-    run(&base, vec![1], 1);
-    run(&base, vec![2], 2);
+    println!("{}", part1(base.clone()));
+    println!("{}", part2(base.clone()));
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_part1() {
+        let program = parse_program("109,1,204,-1,1001,100,1,100,1008,100,16,101,1006,101,0,99"); 
+        let mut c = Computer::new(program);
+
+        let mut results = vec![109,1,204,-1,1001,100,1,100,1008,100,16,101,1006,101,0,99];
+        results.reverse();
+        while let Some(v) = c.run() {
+            let r = results.pop();
+            assert!(r != None);
+            assert!(v == r.unwrap());
+        };
+    }
 }
