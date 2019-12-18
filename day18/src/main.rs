e1bb2cf569942292148837a3dd843a3f1070ed39
Jacques Schutte <jqschutte@gmail.com>
2019-12-18 18:54:18 +0200

    Part 1 Fmt + clippy

--- day18/src/main.rs
+++ day18/src/main.rs
@@ -1,8 +1,8 @@
 use pathfinding::directed::astar::astar;
 use pathfinding::grid::Grid;
 
-use std::collections::HashMap;
 use std::cell::RefCell;
+use std::collections::HashMap;
 
 #[derive(Debug, Default)]
 struct KeyDoor {
@@ -16,7 +16,8 @@ struct EqGrid {
     player: (usize, usize),
 }
 
-fn main() {
+#[allow(dead_code)]
+fn part1() {
     let input = include_str!("../input");
     let width = input.find('\n').unwrap() - 1;
     let height = input.trim().lines().count();
@@ -43,83 +44,89 @@ fn main() {
             }
             'a'..='z' => {
                 original_grid.add_vertex((x, y));
-                key_doors.entry(c).or_insert(Default::default()).key = (x, y);
+                key_doors.entry(c).or_insert_with(Default::default).key = (x, y);
             }
             'A'..='Z' => {
                 key_doors
                     .entry(c.to_lowercase().next().unwrap())
-                    .or_insert(Default::default())
+                    .or_insert_with(Default::default)
                     .door = (x, y);
             }
             _ => panic!("Unhandled character"),
         })
     });
 
-    grid_cache.borrow_mut().insert("".to_string(), original_grid);
+    grid_cache
+        .borrow_mut()
+        .insert("".to_string(), original_grid);
 
-    let path = astar(&original, |g| {
-        key_doors.iter().filter_map(|(k, v)| {
-            if !grid_cache.borrow()[&g.grid_key].has_vertex(&v.door) {
-                let path = astar(
-                    &g.player,
-                    |&p| grid_cache.borrow()[&g.grid_key].neighbours(&p).into_iter().map(|n| (n, 1)),
-                    |&p| grid_cache.borrow()[&g.grid_key].distance(&p, &v.key),
-                    |&p| p == v.key,
-                    );
-                if let Some((_, cost)) = path {
-                    let mut grid = grid_cache.borrow()[&g.grid_key].clone();
-                    grid.add_vertex(v.door);
+    let path = astar(
+        &original,
+        |g| {
+            key_doors
+                .iter()
+                .filter_map(|(k, v)| {
+                    if !grid_cache.borrow()[&g.grid_key].has_vertex(&v.door) {
+                        let path = astar(
+                            &g.player,
+                            |&p| {
+                                grid_cache.borrow()[&g.grid_key]
+                                    .neighbours(&p)
+                                    .into_iter()
+                                    .map(|n| (n, 1))
+                            },
+                            |&p| grid_cache.borrow()[&g.grid_key].distance(&p, &v.key),
+                            |&p| p == v.key,
+                        );
+                        if let Some((_, cost)) = path {
+                            let mut grid = grid_cache.borrow()[&g.grid_key].clone();
+                            grid.add_vertex(v.door);
 
-                    let mut chars: Vec<_> = g.grid_key.chars().collect();
-                    chars.push(*k);
-                    chars.sort();
+                            let mut chars: Vec<_> = g.grid_key.chars().collect();
+                            chars.push(*k);
+                            chars.sort();
 
-                    let grid_key: String = chars.into_iter().collect();
-                    grid_cache.borrow_mut().insert(grid_key.clone(), grid);
+                            let grid_key: String = chars.into_iter().collect();
+                            grid_cache.borrow_mut().insert(grid_key.clone(), grid);
 
-                    return Some((EqGrid {
-                        player: v.key,
-                        grid_key,
-                    }, cost));
-                }
-            }
+                            return Some((
+                                EqGrid {
+                                    player: v.key,
+                                    grid_key,
+                                },
+                                cost,
+                            ));
+                        }
+                    }
 
-            None
-        }).collect::<Vec<_>>()
-    }, |g| {
-        let g_grid = &grid_cache.borrow()[&g.grid_key];
-        key_doors.iter().map(|(_, v)| {
-            if !g_grid.has_vertex(&v.door) {
-                g_grid.distance(&g.player, &v.key)
-            }
-            else {
-                0
-            }
-        }).sum::<usize>()
-    }, |g| {
-        println!("{:?}", g.grid_key);
-        let g_grid = &grid_cache.borrow()[&g.grid_key];
-        //g_grid.has_vertex(&key_doors[&'c'].door)
-        key_doors.iter().all(|(_, v)| g_grid.has_vertex(&v.door))
-    });
+                    None
+                })
+                .collect::<Vec<_>>()
+        },
+        |g| {
+            let g_grid = &grid_cache.borrow()[&g.grid_key];
+            key_doors
+                .iter()
+                .map(|(_, v)| {
+                    if !g_grid.has_vertex(&v.door) {
+                        g_grid.distance(&g.player, &v.key)
+                    } else {
+                        0
+                    }
+                })
+                .sum::<usize>()
+        },
+        |g| {
+            println!("{:?}", g.grid_key);
+            let g_grid = &grid_cache.borrow()[&g.grid_key];
+            //g_grid.has_vertex(&key_doors[&'c'].door)
+            key_doors.iter().all(|(_, v)| g_grid.has_vertex(&v.door))
+        },
+    );
 
     println!("{:?}", path);
-    /*
-    for (k, v) in &key_doors {
-        let ast = astar(
-            &player,
-            |&p| grid.neighbours(&p).into_iter().map(|n| (n, 1)),
-            |&p| grid.distance(&p, &v.key),
-            |&p| p == v.key,
-        );
-        if let Some(path) = ast {
-            println!("{}: {}", k, path.1);
-        }
-        //println!("{}, {:?}, {}", k, grid.neighbours(&player), grid.distance(&player, &v.key));
-    }
-    */
+}
 
-    //println!("{:?}", grid);
-    //println!("{:?}", key_doors);
-    //println!("{:?}", player);
+fn main() {
+    //part1();
 }
