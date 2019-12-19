7ab2eb8a65623a20a1d495e125379a4988136ae5
Jacques Schutte <jqschutte@gmail.com>
2019-12-19 07:14:01 +0200

    Day 19 - Part 1

--- day19/src/main.rs
+++ day19/src/main.rs
@@ -1,3 +1,28 @@
+use intcode::{parse_program, Computer, Program};
+
+use std::collections::HashMap;
+
+fn part1(program: Program) -> usize {
+    let mut grid = HashMap::new();
+
+    for y in 0..=49 {
+        for x in 0..=49 {
+            let mut computer = Computer::new(program.clone());
+
+            computer.push(x);
+            computer.push(y);
+            if let Some(c) = computer.run() {
+                grid.insert((x, y), c);
+            }
+        }
+    }
+
+    grid.iter().filter(|&(_, v)| *v == 1).count()
+}
+
 fn main() {
-    println!("Hello, world!");
+    let input = include_str!("../input");
+    let program = parse_program(input);
+
+    println!("{}", part1(program.clone()));
 }
