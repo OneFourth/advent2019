7e7468158fd267b94a06af48c3d68c56a7b2572d
Jacques Schutte <jqschutte@gmail.com>
2019-12-21 07:33:31 +0200

    Day 21 - Part 1

--- day21/src/main.rs
+++ day21/src/main.rs
@@ -1,11 +1,24 @@
 use intcode::{parse_program, Computer, Program};
+use std::convert::TryFrom;
 
 fn part1(base: Program) {
+    let mut c = Computer::new(base);
+
+    let command = "NOT A J\nNOT B T\nOR T J\nNOT C T\nOR T J\nAND D J\nWALK\n";
+    command.bytes().for_each(|b| c.push(b as i64));
+
+    while let Some(out) = c.run() {
+        if let Ok(c) = u8::try_from(out) {
+            print!("{}", c as char);
+        } else {
+            println!("Damage: {}", out);
+        }
+    }
 }
 
 fn main() {
     let input = include_str!("../input");
     let base = parse_program(input);
 
-    println!("{:?}", part1(base));
+    part1(base);
 }
