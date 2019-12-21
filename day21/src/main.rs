f0cc608e3dc2f8696b517b743598f617bcc767da
Jacques Schutte <jqschutte@gmail.com>
2019-12-21 07:45:43 +0200

    Day 21 - Part 2

--- day21/src/main.rs
+++ day21/src/main.rs
@@ -7,6 +7,7 @@ fn part1(base: Program) {
     let command = "NOT A J\nNOT B T\nOR T J\nNOT C T\nOR T J\nAND D J\nWALK\n";
     command.bytes().for_each(|b| c.push(b as i64));
 
+    println!("Part 1:");
     while let Some(out) = c.run() {
         if let Ok(c) = u8::try_from(out) {
             print!("{}", c as char);
@@ -16,9 +17,28 @@ fn part1(base: Program) {
     }
 }
 
+fn part2(base: Program) {
+    let mut c = Computer::new(base);
+
+    // (!A | !B | !C) & D & (E | H)
+    let command = "NOT A J\nNOT B T\nOR T J\nNOT C T\nOR T J\nAND D J\nNOT H T\nNOT T T\nOR E T\nAND T J\nRUN\n";
+    command.bytes().for_each(|b| c.push(b as i64));
+
+    println!("Part 2:");
+    while let Some(out) = c.run() {
+        if let Ok(c) = u8::try_from(out) {
+            print!("{}", c as char);
+        } else {
+            println!("Damage: {}", out);
+        }
+    }
+}
+
+
 fn main() {
     let input = include_str!("../input");
     let base = parse_program(input);
 
-    part1(base);
+    part1(base.clone());
+    part2(base);
 }
