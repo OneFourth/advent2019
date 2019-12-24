40df757dfbd1a7be9aa0c5bb82f5f5bccedec65d
Jacques Schutte <jqschutte@gmail.com>
2019-12-24 11:21:47 +0200

    Day 24 - Part 2

--- day24/src/main.rs
+++ day24/src/main.rs
@@ -60,6 +60,134 @@ fn part1(state: HashMap<(isize, isize), bool>) -> u32 {
     bio_rating(old_state)
 }
 
+fn part2(state: HashMap<(isize, isize), bool>) -> usize {
+    let mut current_state = state.iter().filter_map(|(&(x, y), &b)| {
+        if !(x == 2 && y == 2) && b {
+            Some((x, y, 0))
+        }
+        else {
+            None
+        }
+    }).collect::<HashSet::<_>>();
+
+    for _ in 0..200 {
+        let mut counts = HashMap::new();
+
+        for &(x, y, d) in &current_state {
+            match (x, y) {
+                (0, 0) => {
+                    *counts.entry((x + 1, y, d)).or_insert(0) += 1;
+                    *counts.entry((x, y + 1, d)).or_insert(0) += 1;
+
+                    *counts.entry((2, 1, d - 1)).or_insert(0) += 1;
+                    *counts.entry((1, 2, d - 1)).or_insert(0) += 1;
+                }
+                (4, 0) => {
+                    *counts.entry((x - 1, y, d)).or_insert(0) += 1;
+                    *counts.entry((x, y + 1, d)).or_insert(0) += 1;
+
+                    *counts.entry((3, 2, d - 1)).or_insert(0) += 1;
+                    *counts.entry((2, 1, d - 1)).or_insert(0) += 1;
+                }
+                (0, 4) => {
+                    *counts.entry((x + 1, y, d)).or_insert(0) += 1;
+                    *counts.entry((x, y - 1, d)).or_insert(0) += 1;
+
+                    *counts.entry((1, 2, d - 1)).or_insert(0) += 1;
+                    *counts.entry((2, 3, d - 1)).or_insert(0) += 1;
+                }
+                (4, 4) => {
+                    *counts.entry((x - 1, y, d)).or_insert(0) += 1;
+                    *counts.entry((x, y - 1, d)).or_insert(0) += 1;
+
+                    *counts.entry((3, 2, d - 1)).or_insert(0) += 1;
+                    *counts.entry((2, 3, d - 1)).or_insert(0) += 1;
+                }
+                (0, _) => {
+                    *counts.entry((1, 2, d - 1)).or_insert(0) += 1;
+                    *counts.entry((x + 1, y, d)).or_insert(0) += 1;
+                    *counts.entry((x, y - 1, d)).or_insert(0) += 1;
+                    *counts.entry((x, y + 1, d)).or_insert(0) += 1;
+                }
+                (4, _) => {
+                    *counts.entry((x - 1, y, d)).or_insert(0) += 1;
+                    *counts.entry((3, 2, d - 1)).or_insert(0) += 1;
+                    *counts.entry((x, y - 1, d)).or_insert(0) += 1;
+                    *counts.entry((x, y + 1, d)).or_insert(0) += 1;
+                }
+                (_, 0) => {
+                    *counts.entry((x - 1, y, d)).or_insert(0) += 1;
+                    *counts.entry((x + 1, y, d)).or_insert(0) += 1;
+                    *counts.entry((2, 1, d - 1)).or_insert(0) += 1;
+                    *counts.entry((x, y + 1, d)).or_insert(0) += 1;
+                }
+                (_, 4) => {
+                    *counts.entry((x - 1, y, d)).or_insert(0) += 1;
+                    *counts.entry((x + 1, y, d)).or_insert(0) += 1;
+                    *counts.entry((x, y - 1, d)).or_insert(0) += 1;
+                    *counts.entry((2, 3, d - 1)).or_insert(0) += 1;
+                }
+                (1, 2) => {
+                    *counts.entry((x - 1, y, d)).or_insert(0) += 1;
+                    *counts.entry((x, y - 1, d)).or_insert(0) += 1;
+                    *counts.entry((x, y + 1, d)).or_insert(0) += 1;
+
+                    for y_ in 0..5 {
+                        *counts.entry((0, y_, d + 1)).or_insert(0) += 1;
+                    }
+                }
+                (3, 2) => {
+                    *counts.entry((x + 1, y, d)).or_insert(0) += 1;
+                    *counts.entry((x, y - 1, d)).or_insert(0) += 1;
+                    *counts.entry((x, y + 1, d)).or_insert(0) += 1;
+
+                    for y_ in 0..5 {
+                        *counts.entry((4, y_, d + 1)).or_insert(0) += 1;
+                    }
+                }
+                (2, 1) => {
+                    *counts.entry((x - 1, y, d)).or_insert(0) += 1;
+                    *counts.entry((x + 1, y, d)).or_insert(0) += 1;
+                    *counts.entry((x, y - 1, d)).or_insert(0) += 1;
+
+                    for x_ in 0..5 {
+                        *counts.entry((x_, 0, d + 1)).or_insert(0) += 1;
+                    }
+                }
+                (2, 3) => {
+                    *counts.entry((x - 1, y, d)).or_insert(0) += 1;
+                    *counts.entry((x + 1, y, d)).or_insert(0) += 1;
+                    *counts.entry((x, y + 1, d)).or_insert(0) += 1;
+
+                    for x_ in 0..5 {
+                        *counts.entry((x_, 4, d + 1)).or_insert(0) += 1;
+                    }
+                }
+                (x, y) => {
+                    *counts.entry((x - 1, y, d)).or_insert(0) += 1;
+                    *counts.entry((x + 1, y, d)).or_insert(0) += 1;
+                    *counts.entry((x, y - 1, d)).or_insert(0) += 1;
+                    *counts.entry((x, y + 1, d)).or_insert(0) += 1;
+                }
+            }
+        }
+
+        current_state = counts.iter().filter_map(|(&p, &c)| {
+            if current_state.contains(&p) {
+                if c == 1 {
+                    return Some(p);
+                }
+            }
+            else if c == 1 || c == 2 {
+                return Some(p);
+            }
+            None
+        }).collect();
+    }
+
+    current_state.len()
+}
+
 fn main() {
     let input = include_str!("../input");
 
@@ -69,5 +197,6 @@ fn main() {
         })
     }).collect();
 
-    println!("{}", part1(state));
+    println!("Part 1: {}", part1(state.clone()));
+    println!("Part 2: {}", part2(state));
 }
